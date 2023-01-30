c4bcfe0cff63b5ede84dd8c8cdf26098d787fe94
Willington75 <willington752012@gmail.com>
2023-01-30 12:00:17 -0500

    Update Email

--- src/main/java/com/demo/serenity/is/testing/util/User.java
+++ src/main/java/com/demo/serenity/is/testing/util/User.java
@@ -4,8 +4,8 @@ public enum User {
 
     FULL_NAME("WillingtonDiaz"),
     FULL_NAME2("admin"),
-    EMAIL("WillingtonDiaz21234@gmail.com"),
-    CONFIRM_EMAIL("WillingtonDiaz21234@gmail.com"),
+    EMAIL("WillingtonDiaz20234@gmail.com"),
+    CONFIRM_EMAIL("WillingtonDiaz20234@gmail.com"),
     PASSWORD("Colombia2023"),
     PASSWORD2("serenity"),
     CONFIRM_PASSWORD("Colombia2023");
